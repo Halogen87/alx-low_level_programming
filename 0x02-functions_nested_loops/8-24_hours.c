0cfcfce0628aeb5ad97aa31ccfd7a2fb85e46d84
Olusegun Adegoke <adegokesegun86@gmail.com>
2022-09-14 20:38:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,17 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (minutes = 0;minutes <=59; minutes++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0')
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
+		}
 	}
-}
 }
 	
