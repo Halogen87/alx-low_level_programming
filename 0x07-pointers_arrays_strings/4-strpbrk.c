e00c1790b6553b9242030290d7a471e83d979de7
Olusegun Adegoke <adegokesegun86@gmail.com>
2022-09-26 15:51:58 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,8 +13,10 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
-			return (s + i);
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
 		}
 	}
 	return (NULL);
