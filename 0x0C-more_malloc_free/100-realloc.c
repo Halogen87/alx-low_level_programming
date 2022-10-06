72dd49aea54305dcdd3ba2772884d9f51b79add3
Olusegun Adegoke <adegokesegun86@gmail.com>
2022-10-06 13:55:20 -0700

    range

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,5 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (i = 0; i < old_size; i++)
 		new_location[i] = new_ptr[i];
 	free(ptr);
+}
+
 	return (new_location);
 }
